f488bb4bf4809c39a23b9096da5a53e916675efa
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-11-12 11:24:26 +0700

    add qr image route

--- routes/media.route.js
+++ routes/media.route.js
@@ -4,9 +4,11 @@ const {
   storageImage,
   storageVideo,
   storageFile,
+  generateQR,
 } = require("../controller/media.controller");
 
 router.post("/image", storage.image.single("images"), storageImage);
 router.post("/video", storage.video.single("videos"), storageVideo);
 router.post("/file", storage.file.single("files"), storageFile);
+router.post("/generate-qr", generateQR);
 module.exports = router;
